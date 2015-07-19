e0ba09be4ca6ac277082ba69300a7496ef983922
maurya <maurya.wolverine@gmail.com>
2015-07-19 01:25:09 -0400

    observation period and modifications to make data compatible

--- omod/src/main/java/org/openmrs/module/ohdsi/web/DownloadInsertStatementsServlet.java
+++ omod/src/main/java/org/openmrs/module/ohdsi/web/DownloadInsertStatementsServlet.java
@@ -28,8 +28,8 @@ public class DownloadInsertStatementsServlet extends HttpServlet {
         try {
 
             String s = new SimpleDateFormat("dMy_Hm").format(new Date());
-            response.setHeader("Content-Type", "text/plain;charset=UTF-8");
-            response.setHeader("Content-Disposition", "attachment; filename=ohdsiInserts" + s + ".txt");
+            response.setHeader("Content-Type", "text/sql;charset=UTF-8");
+            response.setHeader("Content-Disposition", "attachment; filename=ohdsiInserts" + s + ".sql");
             //String headerLine = "Concept Id,Name,Description,Synonyms,Answers,Set Members,Class,Datatype,Changed By,Creator\n";
             //response.getWriter().write(headerLine);
 
@@ -86,13 +86,14 @@ public class DownloadInsertStatementsServlet extends HttpServlet {
                         completeString=completeString+","+year+","+month+","+day;
                     }
                     else
-                        completeString=completeString+",null,null,null";
+                        continue;
+                    //time
+                    completeString=completeString+",null";
                     //race
                     completeString=completeString+",2060-2";
                     //ethnicity
                     completeString=completeString+",0";
-                    //gendersource
-                    completeString=completeString+","+rs.getString("gender");
+
                     //location_id,provider_id,care_site_id,
                     completeString=completeString+",null,null,null";
                     //person source value
@@ -102,15 +103,49 @@ public class DownloadInsertStatementsServlet extends HttpServlet {
                     while(patientidentifierresutset.next()) {
                         identifier = patientidentifierresutset.getString("identifier");
                     }
-                    completeString=completeString+","+rs.getInt("person_id")+identifier;
-                    //gender_source_value,gender_source_concept_id,race_source_value,race_source_concept_id,ethnicity_source_value,ethnicity_source_concept_id
-                    completeString=completeString+",null,null,null,null,null,null";
+                    completeString=completeString+",'"+rs.getInt("person_id")+identifier+"'";
+                    //gendersource
+                    if(rs.getString("gender")!=null)
+                    completeString=completeString+",'"+rs.getString("gender")+"'";
+                    else
+                        completeString=completeString+",null";
+                    //gender_source_concept_id,race_source_value,race_source_concept_id,ethnicity_source_value,ethnicity_source_concept_id
+                    completeString=completeString+",null,null,null,null,null";
                     completeString=completeString+");";
 
                     response.getWriter().write(completeString);
                     //System.out.println(completeString);
                 }
                 rs.close();
+                //obs table population
+
+                sql = "SELECT DISTINCT person_id from openmrs.obs;";
+                rs = stmt.executeQuery(sql);
+                int counter=0;
+                while(rs.next()) {
+                    counter++;
+                    completeString="\nINSERT INTO observation_period (observation_period_id,person_id,observation_period_start_date,observation_period_end_date,period_type_concept_id)" +
+                            "VALUES ("+counter+","+rs.getInt("person_id");
+                    String obsStartDatesql = "select min(obs_datetime) from obs where person_id="+rs.getInt("person_id")+";";
+                    ResultSet obsStartDateresutset = stmt2.executeQuery(obsStartDatesql);
+                    Date obsStartDate=null;
+                    while(obsStartDateresutset.next()) {
+                        obsStartDate = obsStartDateresutset.getDate("min(obs_datetime)");
+                    }
+                    completeString=completeString+",'"+obsStartDate+"'";
+
+                    String obsEndDatesql = "select max(obs_datetime) from obs where person_id="+rs.getInt("person_id")+";";
+                    ResultSet obsEndDateresutset = stmt2.executeQuery(obsEndDatesql);
+                    Date obsEndDate=null;
+                    while(obsEndDateresutset.next()) {
+                        obsEndDate = obsEndDateresutset.getDate("max(obs_datetime)");
+                    }
+                    completeString=completeString+",'"+obsEndDate+"'";
+                    completeString=completeString+",0";
+                    completeString=completeString+");";
+
+                    response.getWriter().write(completeString);
+                }
             }catch(SQLException se){
                 //Handle errors for JDBC
                 se.printStackTrace();
